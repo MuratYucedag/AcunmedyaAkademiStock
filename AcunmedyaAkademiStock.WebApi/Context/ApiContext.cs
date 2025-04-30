3d0d9562ecb2765259e71db5079a5f1ebc15505c
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-30 14:23:05 +0300

    Add project files.

---
+++ AcunmedyaAkademiStock.WebApi/Context/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using AcunmedyaAkademiStock.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunmedyaAkademiStock.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial catalog=AcunmedyaApiDb;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+//trust server certificate=true;
