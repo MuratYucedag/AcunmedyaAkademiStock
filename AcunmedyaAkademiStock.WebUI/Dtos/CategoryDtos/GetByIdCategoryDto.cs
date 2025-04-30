2d31e8543d126e1ca404a707622f11e483d97a17
MuratYucedag <yucedagmurat23@gmail.com>
2025-04-30 14:58:26 +0300

    ....

---
+++ AcunmedyaAkademiStock.WebUI/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AcunmedyaAkademiStock.WebUI.Dtos.CategoryDtos
+{
+    public class GetByIdCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
